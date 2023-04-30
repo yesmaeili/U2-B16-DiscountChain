7ca67963ee7a94a15a575a07c97a005a6b25ff27
Yasaman Esmaeili <yasamanesmaeili@Yasamans-MacBook-Air.local>
2023-04-30 19:06:26 -0400

    feat:updates added

---
+++ index.js
@@ -0,0 +1,74 @@
+// Customers' information per the homework.
+const timmy = {
+  prescription: "acetaminophen",
+  pricePerRefill: 25,
+  refills: 3,
+  subscription: false,
+  coupon: true,
+};
+
+const sarah = {
+  prescription: "diphenhydramine",
+  pricePerRefill: 50,
+  refills: 1,
+  subscription: true,
+  coupon: false,
+};
+
+const rocky = {
+  prescription: "phenylephrine",
+  pricePerRefill: 30,
+  refills: 5,
+  subscription: true,
+  coupon: true,
+};
+
+// Create a function that calculates customers' total refill price.
+function calculateRefillTotal(customer) {
+  return customer.pricePerRefill * customer.refills;
+}
+
+// Create a function to calculate 25% discount per customers' subscription.
+function calculateSubscriptionDiscount(customer, total) {
+  if (customer.subscription) {
+    return total - total * 0.25;
+  } else {
+    return total;
+  }
+}
+
+// Create a function to calculate $10 discount per customers' subscription.
+function calculateCouponDiscount(customer, discountedCost) {
+  if (customer.coupon) {
+    return discountedCost - 10;
+  } else {
+    return discountedCost;
+  }
+}
+
+// Creat a function to calculate total discount provided.
+function calculateGrandTotal(customer) {
+  const refillTotal = calculateRefillTotal(customer);
+  const subscriptionDiscountedTotal = calculateSubscriptionDiscount(
+    customer,
+    refillTotal
+  );
+  const couponDiscountedTotal = calculateCouponDiscount(
+    customer,
+    subscriptionDiscountedTotal
+  );
+  return couponDiscountedTotal;
+}
+
+// Create a function to calculate grandtotal for all customers.
+function newFunction() {
+  const timmyGrandTotal = calculateGrandTotal(timmy);
+  const sarahGrandTotal = calculateGrandTotal(sarah);
+  const rockyGrandTotal = calculateGrandTotal(rocky);
+  return { timmyGrandTotal, sarahGrandTotal, rockyGrandTotal };
+}
+
+// Print results.
+console.log(`Timmy's grand total is $${timmyGrandTotal}.`);
+console.log(`Sarah's grand total is $${sarahGrandTotal}.`);
+console.log(`Rocky's grand total is $${rockyGrandTotal}.`);
